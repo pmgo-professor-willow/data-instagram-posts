74312955ab04b059e1196160ef5cc0072dd88b34
Ze-Hao Wang <salmon.zh.tw@gmail.com>
2023-08-12 14:45:41 +0800

    Change source to picuki

--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,67 @@
 // Node modules.
 import _ from 'lodash';
+import { Page } from 'puppeteer';
+import puppeteer from 'puppeteer-extra';
 import { parse } from 'node-html-parser';
-import puppeteer, { Page } from 'puppeteer';
 import { mkdirp, writeFile } from 'fs-extra';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 // Local modules.
-import { hostUrl } from './utils';
+import { download, hostUrl } from './utils';
 import IG_USERS from '../data/instagram-users.json';
 
 interface Post {
   contentText: string;
-  timestamp: string;
+  timeText: string;
   imageLinks: string[];
 };
 
-const getPost = async (page: Page, path: string): Promise<Post> => {
-  const url = new URL(path, hostUrl).href;
-  await page.goto(url, { waitUntil: 'networkidle0' });
+const getPost = async (page: Page, url: string): Promise<Post> => {
+  await page.goto(url, { waitUntil: 'domcontentloaded' });
 
-  let contentText = '';
-  let datetimeText = '';
-  const imageLinkSet = new Set<string>();
-
-  for (const _time of _.range(10)) {
-    const xml = await page.evaluate(() => document.querySelector('*')?.outerHTML!);
-    const root = parse(xml);
-    const imageItems = root.querySelectorAll('[role=presentation] [role=presentation] img[srcset]');
-    const hasNextButton = root.querySelector('[role=presentation] button[aria-label=Next]');
-
-    contentText = root.querySelector('[role=presentation] [role=presentation] h1')?.structuredText ?? '';
-    datetimeText = root.querySelector('[role=presentation] [role=presentation] a time')?.getAttribute('datetime') ?? '';
+  // 'https://www.picuki.com/media/3167389780732852671' => '3167389780732852671'
+  const mediaId = url.split('/').pop()!;
   
-    imageItems.forEach((imageItem) => imageLinkSet.add(imageItem.getAttribute('src')!));
-  
-    if (hasNextButton) {
-      await new Promise((resolve) => setTimeout(resolve, 5000));
-      await page.click('[role=presentation] button[aria-label=Next]');
-    } else {
-      break;
-    }
+  const xml = await page.evaluate(() => document.querySelector('*')?.outerHTML!);
+  const root = parse(xml);
+  const contentText = root.querySelector('.single-photo-description')?.text ?? '';
+  const timeText = root.querySelector('.single-photo-time')?.text ?? '';
+  const rawImageLinks = root.querySelectorAll('.single-photo.owl-carousel .owl-item img, .single-photo img')
+    .map((imageItem) => imageItem.getAttribute('src')!);
+
+  // Download images
+  const imageLinks: string[] = [];
+  for (let i = 0; i < rawImageLinks.length; i++) {
+    const storageBaseUrl = 'https://raw.githubusercontent.com/pmgo-professor-willow/data-instagram-posts/gh-pages/';
+    const filename = `${mediaId}_${i+1}.jpg`;
+    const imagePath = `./artifacts/${filename}`;
+    imageLinks.push(new URL(filename, storageBaseUrl).href);
+
+    await download(rawImageLinks[i], imagePath);
   }
-
-  const imageLinks = Array.from(imageLinkSet);
 
   return {
     contentText,
-    timestamp: new Date(datetimeText).toISOString(),
+    timeText,
     imageLinks,
   };
 };
 
 const getPosts = async (page: Page, userId: string): Promise<Post[]> => {
-  const url = new URL(userId, hostUrl).href;
-  await page.goto(url, { waitUntil: 'networkidle0' });
+  const url = new URL(`profile/${userId}`, hostUrl).href;
+  await page.goto(url, { waitUntil: 'domcontentloaded', timeout: 180000 });
+
   const xml = await page.evaluate(() => document.querySelector('*')?.outerHTML!);
 
   const root = parse(xml);
-  const linkItems = root.querySelectorAll('article > div > div > div > div > a');
+  const linkItems = root.querySelectorAll('.box-photo[data-s=media] .photo a');
 
   const links = linkItems.map((linkItem) => linkItem.getAttribute('href')!);
 
   const posts: Post[] = [];
 
-  // FIXME: For testing.
-  for (const link of links.slice(0, 6)) {
+  for (const link of links.slice(0, 10)) {
+    console.log(`Current link: ${link}`);
+
     const post = await getPost(page, link);
     posts.push(post);
 
@@ -76,7 +75,7 @@ const main = async () => {
   const outputPath = './artifacts';
   await mkdirp(outputPath);
 
-  const browser = await puppeteer.launch({
+  const browser = await puppeteer.use(StealthPlugin()).launch({
     args: ['--no-sandbox'],
     executablePath: process.env.PUPPETEER_EXEC_PATH, // set by docker container
     headless: false,
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,26 @@
-const hostUrl = 'https://www.instagram.com/';
+// Node modules.
+import https from 'https';
+import fs from 'fs-extra';
+
+const hostUrl = 'https://www.picuki.com/';
+
+const download = (url: string, destination: string) => new Promise((resolve, reject) => {
+  const file = fs.createWriteStream(destination);
+
+  https.get(url, response => {
+    response.pipe(file);
+
+    file.on('finish', () => {
+      file.close();
+      resolve(true);
+    });
+  }).on('error', error => {
+    fs.unlink(destination);
+    reject(error.message);
+  });
+});
 
 export {
   hostUrl,
+  download,
 };
